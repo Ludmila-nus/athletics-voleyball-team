cd75f227566c184a187bbaae06186cda9dc1dd79
Ludmila-nus <ludmilanussio@hotmail.com>
2024-03-29 01:24:41 -0300

    editing the homepage

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import "./sass/style.scss";
 
 
-
+// titulo 
 let titulo = document.getElementById('title');
 let texto = titulo.textContent;
 titulo.textContent = ''; 
@@ -20,6 +20,17 @@ function mostrarLetra() {
 // Llamar a la función para empezar a mostrar las letras
 mostrarLetra();
 
+// menu
+document.addEventListener('DOMContentLoaded', function(){
+    const toggleMenuBtn = document.getElementById('toggleMenu');
+    const menu = document.getElementsByClassName('header-principal__nav');
+
+    toggleMenuBtn.addEventListener('click', function(){
+        // Alternar la visibilidad del menú cambiando la clase 'visible' con toggle hace lo contario(si la tiene la borra, sino la agrega)
+        menu.classList.toggle('visible');
+    });
+
+});
 
 
 
