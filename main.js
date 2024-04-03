c97dbc78f6ae8bcc6e23e4108f904e2688201121
Ludmila-nus <ludmilanussio@hotmail.com>
2024-04-03 11:41:47 -0300

    Added changes to the navigation menu

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 import "./sass/style.scss";
 
 
-// titulo 
+// ------- Title ----------
 let titulo = document.getElementById('title');
 let texto = titulo.textContent;
 titulo.textContent = ''; 
 
+// The letters appear one by one
 const tiempoEntreLetras = 100; 
 let index = 0;
 
@@ -17,16 +18,38 @@ function mostrarLetra() {
     }
 }
 
-// Llamar a la función para empezar a mostrar las letras
+// Call the function to start displaying the letters
 mostrarLetra();
 
-// menu
+// ------- Login----------
+let modal = document.getElementById('loginModal');
+let loginButton = document.getElementById('loginButton');
+let closeButton = document.getElementsByClassName('close')[0];
+
+// Add a click event to the login button to open the modal
+loginButton.addEventListener('click', function(){
+    modal.style.display = 'block';
+})
+
+// When the user clicks on <span> (x), close the modal
+closeButton.addEventListener('click', function(){
+    modal.style.display = 'none';
+})
+
+// When the user clicks anywhere outside the modal, close it
+window.addEventListener('click', function(event) {
+    if (event.target == modal) {
+      modal.style.display = 'none';
+    }
+  })
+
+//------- menu------------
 document.addEventListener('DOMContentLoaded', function(){
     const toggleMenuBtn = document.getElementById('toggleMenu');
-    const menu = document.getElementsByClassName('header-principal__nav');
+    const menu = document.getElementById('menu');
 
     toggleMenuBtn.addEventListener('click', function(){
-        // Alternar la visibilidad del menú cambiando la clase 'visible' con toggle hace lo contario(si la tiene la borra, sino la agrega)
+        // Toggle menu visibility by changing the 'visible' class
         menu.classList.toggle('visible');
     });
 
